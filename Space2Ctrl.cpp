b8e62326993044cdfccf42e7f75db22feacc1845
silgon <silgon3200@gmail.com>
2017-09-17 23:05:56 +0200

    modified to try with o letter in dvorak keyboard

--- Space2Ctrl.cpp
+++ Space2Ctrl.cpp
@@ -28,266 +28,248 @@
 using namespace std;
 
 struct CallbackClosure {
-  Display *ctrlDisplay;
-  Display *dataDisplay;
-  int curX;
-  int curY;
-  void *initialObject;
+    Display *ctrlDisplay;
+    Display *dataDisplay;
+    int curX;
+    int curY;
+    void *initialObject;
 };
 
 typedef union {
-  unsigned char type;
-  xEvent event;
-  xResourceReq req;
-  xGenericReply reply;
-  xError error;
-  xConnSetupPrefix setup;
+    unsigned char type;
+    xEvent event;
+    xResourceReq req;
+    xGenericReply reply;
+    xError error;
+    xConnSetupPrefix setup;
 } XRecordDatum;
+
+class Key
+{
+public:
+    int id;  // id that is pressed
+    int r_id;  // replacement id
+    bool down;
+    struct timeval start;
+    Key(int id, int r_id): id(id), r_id(r_id), down(false){
+    }
+    // virtual ~Key();
+};
+
 
 class Space2Ctrl {
 
-  string m_displayName;
-  CallbackClosure userData;
-  std::pair<int,int> recVer;
-  XRecordRange *recRange;
-  XRecordClientSpec recClientSpec;
-  XRecordContext recContext;
+    string m_displayName;
+    CallbackClosure userData;
+    std::pair<int,int> recVer;
+    XRecordRange *recRange;
+    XRecordClientSpec recClientSpec;
+    XRecordContext recContext;
 
-  void setupXTestExtension(){
-    int ev, er, ma, mi;
-    if (!XTestQueryExtension(userData.ctrlDisplay, &ev, &er, &ma, &mi)) {
-      cout << "%sThere is no XTest extension loaded to X server.\n" << endl;
-      throw exception();
-    }
-  }
-
-  void setupRecordExtension() {
-    XSynchronize(userData.ctrlDisplay, True);
-
-    // Record extension exists?
-    if (!XRecordQueryVersion(userData.ctrlDisplay, &recVer.first, &recVer.second)) {
-      cout << "%sThere is no RECORD extension loaded to X server.\n"
-        "You must add following line:\n"
-        "   Load  \"record\"\n"
-        "to /etc/X11/xorg.conf, in section `Module'.%s" << endl;
-      throw exception();
+    void setupXTestExtension(){
+        int ev, er, ma, mi;
+        if (!XTestQueryExtension(userData.ctrlDisplay, &ev, &er, &ma, &mi)) {
+            cout << "%sThere is no XTest extension loaded to X server.\n" << endl;
+            throw exception();
+        }
     }
 
-    recRange = XRecordAllocRange ();
-    if (!recRange) {
-      // "Could not alloc record range object!\n";
-      throw exception();
-    }
-    recRange->device_events.first = KeyPress;
-    recRange->device_events.last = ButtonRelease;
-    recClientSpec = XRecordAllClients;
+    void setupRecordExtension() {
+        XSynchronize(userData.ctrlDisplay, True);
 
-    // Get context with our configuration
-    recContext = XRecordCreateContext(userData.ctrlDisplay, 0, &recClientSpec, 1, &recRange, 1);
-    if (!recContext) {
-      cout << "Could not create a record context!" << endl;
-      throw exception();
-    }
-  }
-
-  static int diff_ms(timeval t1, timeval t2) {
-    return ( ((t1.tv_sec - t2.tv_sec) * 1000000)
-             + (t1.tv_usec - t2.tv_usec) ) / 1000;
-  }
-
-  // Called from Xserver when new event occurs.
-  static void eventCallback(XPointer priv, XRecordInterceptData *hook) {
-
-    if (hook->category != XRecordFromServer) {
-      XRecordFreeData(hook);
-      return;
-    }
-
-    CallbackClosure *userData = (CallbackClosure *) priv;
-    XRecordDatum *data = (XRecordDatum *) hook->data;
-    static bool space_down = false;
-    static bool key_combo = false;
-    static bool modifier_down = false;
-    static struct timeval startWait, endWait;
-
-    unsigned char t = data->event.u.u.type;
-    int c = data->event.u.u.detail;
-
-    // cout << "\nState:" << endl;
-    // if (space_down)
-    //   cout << "space_down = true" << endl;
-    // else
-    //   cout << "space_down = false" << endl;
-
-    // if (key_combo)
-    //   cout << "key_combo = true" << endl;
-    // else
-    //   cout << "key_combo = false" << endl;
-
-    // // if (modifier_down)
-    // //   cout << "modifier_down = true" << endl;
-    // // else
-    // //   cout << "modifier_down = false" << endl;
-    // cout << endl;
-
-    switch (t) {
-    case KeyPress:
-      {
-        // cout << "KeyPress";
-        if (c == 65) {
-          space_down = true; // space pressed
-          gettimeofday(&startWait, NULL);
-          // cout << "    65" << endl;
-
-        } else if ( (c == XKeysymToKeycode(userData->ctrlDisplay, XK_Control_L))
-                    || (c == XKeysymToKeycode(userData->ctrlDisplay, XK_Control_R)) ) {
-          // cout << "    Control_{L||R}" << endl;
-          // ctrl pressed
-          if (space_down) { // space ctrl sequence
-            XTestFakeKeyEvent(userData->ctrlDisplay, 255, True, CurrentTime);
-            XTestFakeKeyEvent(userData->ctrlDisplay, 255, False, CurrentTime);
-          }
-        } else if ( (c == XKeysymToKeycode(userData->ctrlDisplay, XK_Shift_L))
-                    || (c == XKeysymToKeycode(userData->ctrlDisplay, XK_Shift_R))
-                    || (c == 108)
-                    ) {
-          // cout << "    Modifier" << endl;
-          // TODO: Find a better way to get those modifiers!!!
-          modifier_down = true;
-
-        } else { // another key pressed
-          // cout << "    Another" << endl;
-          if (space_down) {
-            key_combo = true;
-          } else {
-            key_combo = false;
-          }
-
+        // Record extension exists?
+        if (!XRecordQueryVersion(userData.ctrlDisplay, &recVer.first, &recVer.second)) {
+            cout << "%sThere is no RECORD extension loaded to X server.\n"
+                "You must add following line:\n"
+                "   Load  \"record\"\n"
+                "to /etc/X11/xorg.conf, in section `Module'.%s" << endl;
+            throw exception();
         }
 
-        break;
-      }
-    case KeyRelease:
-      {
-        // cout << "KeyRelease";
-        if (c == 65) {
-          // cout << "    65";
-          space_down = false; // space released
+        recRange = XRecordAllocRange ();
+        if (!recRange) {
+            // "Could not alloc record range object!\n";
+            throw exception();
+        }
+        recRange->device_events.first = KeyPress;
+        recRange->device_events.last = ButtonRelease;
+        recClientSpec = XRecordAllClients;
 
-          if (!key_combo && !modifier_down) {
-            // cout << "    (!key_combo && !modifier_down)";
-            gettimeofday(&endWait, NULL);
-            if ( diff_ms(endWait, startWait) < 600 ) {
-              // if minimum timeout elapsed since space was pressed
-              XTestFakeKeyEvent(userData->ctrlDisplay, 255, True, CurrentTime);
-              XTestFakeKeyEvent(userData->ctrlDisplay, 255, False, CurrentTime);
+        // Get context with our configuration
+        recContext = XRecordCreateContext(userData.ctrlDisplay, 0, &recClientSpec, 1, &recRange, 1);
+        if (!recContext) {
+            cout << "Could not create a record context!" << endl;
+            throw exception();
+        }
+    }
+
+    static int diff_ms(timeval t1, timeval t2) {
+        return ( ((t1.tv_sec - t2.tv_sec) * 1000000)
+                 + (t1.tv_usec - t2.tv_usec) ) / 1000;
+    }
+
+    // Called from Xserver when new event occurs.
+    static void eventCallback(XPointer priv, XRecordInterceptData *hook) {
+
+        if (hook->category != XRecordFromServer) {
+            XRecordFreeData(hook);
+            return;
+        }
+
+        CallbackClosure *userData = (CallbackClosure *) priv;
+        XRecordDatum *data = (XRecordDatum *) hook->data;
+        static Key k_o = Key(39, 252);
+        // static bool k_o.down = false;
+        // static bool key_combo = false;
+        static bool c_right= false, c_left=false, a_left = false;
+        static bool r_ctrl, f_ctrl, r_alt, f_alt; // real and fake control and alt
+        // static bool modifier_down = false;
+        // static struct timeval startWait, endWait;
+
+        unsigned char t = data->event.u.u.type;
+        int c = data->event.u.u.detail;
+
+        // cout << "\nState:" << c << endl;
+        // if (k_o.down)
+        //   cout << "k_o.down = true" << endl;
+        // else
+        //   cout << "k_o.down = false" << endl;
+
+        // if (key_combo)
+        //   cout << "key_combo = true" << endl;
+        // else
+        //   cout << "key_combo = false" << endl;
+
+        // // if (modifier_down)
+        // //   cout << "modifier_down = true" << endl;
+        // // else
+        // //   cout << "modifier_down = false" << endl;
+        // cout << endl;
+
+        switch (t) {
+        case KeyPress:
+            {
+                // verify the first modifiers
+                if(c == XKeysymToKeycode(userData->ctrlDisplay, XK_Control_L))
+                    c_left = true;
+                else if(c == XKeysymToKeycode(userData->ctrlDisplay, XK_Control_R))
+                    c_right = true;
+                else if(c == XKeysymToKeycode(userData->ctrlDisplay, XK_Alt_L))
+                    a_left = true;
+                // get control and r_alt variable
+                r_ctrl = c_left||c_right;
+                f_ctrl = k_o.down;
+                if (c == k_o.id && r_ctrl){
+                    XTestFakeKeyEvent(userData->ctrlDisplay, k_o.r_id, True, CurrentTime);
+                    XTestFakeKeyEvent(userData->ctrlDisplay, k_o.r_id, False, CurrentTime);
+                }
+                else if (c == k_o.id && f_ctrl){
+                    XTestFakeKeyEvent(userData->ctrlDisplay, k_o.r_id, True, CurrentTime);
+                    XTestFakeKeyEvent(userData->ctrlDisplay, k_o.r_id, False, CurrentTime);
+                }
+
+
+                break;
             }
-          }
-          key_combo = false;
-          // cout << endl;
-        } else if ( (c == XKeysymToKeycode(userData->ctrlDisplay, XK_Control_L))
-                    || (c == XKeysymToKeycode(userData->ctrlDisplay, XK_Control_R)) ) {
-          // cout << "    Control_{L||R}" << endl;
-          // ctrl release
-          if (space_down)
-            key_combo = true;
-        } else if ( (c == XKeysymToKeycode(userData->ctrlDisplay, XK_Shift_L))
-                    || (c == XKeysymToKeycode(userData->ctrlDisplay, XK_Shift_R))
-                    || (c == 108)
-                    ) {
-          // cout << "    Modifier" << endl;
-          // TODO: Find a better way to get those modifiers!!!
-          modifier_down = false;
+        case KeyRelease:
+            {
+                if(c == XKeysymToKeycode(userData->ctrlDisplay, XK_Control_L))
+                    c_left = false;
+                else if(c == XKeysymToKeycode(userData->ctrlDisplay, XK_Control_R))
+                    c_right = false;
+                else if(c == XKeysymToKeycode(userData->ctrlDisplay, XK_Alt_L))
+                    a_left = false;
+                else if(c==k_o.id)
+                    k_o.down=false;
+
+                break;
+            }
+        case ButtonPress:
+            {
+
+                // if (k_o.down) {
+                //     key_combo = true;
+                // } else {
+                //     key_combo = false;
+                // }
+
+                // break;
+            }
         }
 
-        break;
-      }
-    case ButtonPress:
-      {
-
-        if (space_down) {
-          key_combo = true;
-        } else {
-          key_combo = false;
-        }
-
-        break;
-      }
+        XRecordFreeData(hook);
     }
-
-    XRecordFreeData(hook);
-  }
 
 public:
-  Space2Ctrl() { }
-  ~Space2Ctrl() {
-    stop();
-  }
-
-  bool connect(string displayName) {
-    m_displayName = displayName;
-    if (NULL == (userData.ctrlDisplay = XOpenDisplay(m_displayName.c_str())) ) {
-      return false;
-    }
-    if (NULL == (userData.dataDisplay = XOpenDisplay(m_displayName.c_str())) ) {
-      XCloseDisplay(userData.ctrlDisplay);
-      userData.ctrlDisplay = NULL;
-      return false;
+    Space2Ctrl() { }
+    ~Space2Ctrl() {
+        stop();
     }
 
-    // You may want to set custom X error handler here
+    bool connect(string displayName) {
+        m_displayName = displayName;
+        if (NULL == (userData.ctrlDisplay = XOpenDisplay(m_displayName.c_str())) ) {
+            return false;
+        }
+        if (NULL == (userData.dataDisplay = XOpenDisplay(m_displayName.c_str())) ) {
+            XCloseDisplay(userData.ctrlDisplay);
+            userData.ctrlDisplay = NULL;
+            return false;
+        }
 
-    userData.initialObject = (void *) this;
-    setupXTestExtension();
-    setupRecordExtension();
+        // You may want to set custom X error handler here
 
-    return true;
-  }
+        userData.initialObject = (void *) this;
+        setupXTestExtension();
+        setupRecordExtension();
 
-  void start() {
-    // // Remap keycode 255 to Keysym space:
-    // KeySym spc = XK_space;
-    // XChangeKeyboardMapping(userData.ctrlDisplay, 255, 1, &spc, 1);
-    // XFlush(userData.ctrlDisplay);
-
-    // TODO: document why the following event is needed
-    XTestFakeKeyEvent(userData.ctrlDisplay, 255, True, CurrentTime);
-    XTestFakeKeyEvent(userData.ctrlDisplay, 255, False, CurrentTime);
-
-    if (!XRecordEnableContext(userData.dataDisplay, recContext, eventCallback,
-                              (XPointer) &userData)) {
-      throw exception();
+        return true;
     }
-  }
 
-  void stop() {
-    if (!XRecordDisableContext (userData.ctrlDisplay, recContext)) {
-      throw exception();
+    void start() {
+        // // Remap keycode 255 to Keysym space:
+        // KeySym spc = XK_space;
+        // XChangeKeyboardMapping(userData.ctrlDisplay, 255, 1, &spc, 1);
+        // XFlush(userData.ctrlDisplay);
+
+        // TODO: document why the following event is needed
+        XTestFakeKeyEvent(userData.ctrlDisplay, 255, True, CurrentTime);
+        XTestFakeKeyEvent(userData.ctrlDisplay, 255, False, CurrentTime);
+
+        if (!XRecordEnableContext(userData.dataDisplay, recContext, eventCallback,
+                                  (XPointer) &userData)) {
+            throw exception();
+        }
     }
-  }
+
+    void stop() {
+        if (!XRecordDisableContext (userData.ctrlDisplay, recContext)) {
+            throw exception();
+        }
+    }
 
 };
 
 Space2Ctrl* space2ctrl;
 
 void stop(int param) {
-  delete space2ctrl;
-  if(param == SIGTERM)
-    cout << "-- Terminating Space2Ctrl --" << endl;
-  exit(1);
+    delete space2ctrl;
+    if(param == SIGTERM)
+        cout << "-- Terminating Space2Ctrl --" << endl;
+    exit(1);
 }
 
 int main() {
-  cout << "-- Starting Space2Ctrl --" << endl;
-  space2ctrl = new Space2Ctrl();
+    cout << "-- Starting Space2Ctrl --" << endl;
+    space2ctrl = new Space2Ctrl();
 
-  void (*prev_fn)(int);
+    void (*prev_fn)(int);
 
-  prev_fn = signal (SIGTERM, stop);
-  if (prev_fn==SIG_IGN) signal (SIGTERM,SIG_IGN);
+    prev_fn = signal (SIGTERM, stop);
+    if (prev_fn==SIG_IGN) signal (SIGTERM,SIG_IGN);
 
-  if (space2ctrl->connect(":0")) {
-    space2ctrl->start();
-  }
-  return 0;
+    if (space2ctrl->connect(":0")) {
+        space2ctrl->start();
+    }
+    return 0;
 }
